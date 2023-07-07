60c56964cd07cf3e7b94587edd17b7768fbbbfa2
C-ArenA <carlostata.ca@gmail.com>
2023-07-07 17:01:28 -0400

    Improved the Expo Observer to use auth facade

--- app/Observers/ExpoObserver.php
+++ app/Observers/ExpoObserver.php
@@ -4,13 +4,17 @@ namespace App\Observers;
 
 use App\Models\Expo;
 use App\Models\User;
+use Illuminate\Support\Facades\Auth;
 
 class ExpoObserver
 {
 
     public function creating(Expo $expo): void{
-        $expo->created_by = User::where('email', 'admin@jucux.com')->first()->id;
-        $expo->updated_by = User::where('email', 'admin@jucux.com')->first()->id;
+        $expo->created_by = Auth::id();
+        $expo->updated_by = Auth::id();
+    }
+    public function updating(Expo $expo): void{
+        $expo->updated_by = Auth::id();
     }
     /**
      * Handle the Expo "created" event.
