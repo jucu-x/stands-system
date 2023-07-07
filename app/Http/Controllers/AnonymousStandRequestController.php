8782be90fa5e27aedb6410d06e1c23d79eed985b
C-ArenA <carlostata.ca@gmail.com>
2023-07-07 04:11:52 -0400

    Models initialized

---
+++ app/Http/Controllers/AnonymousStandRequestController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\AnonymousStandRequest;
+use Illuminate\Http\Request;
+
+class AnonymousStandRequestController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(AnonymousStandRequest $anonymousStandRequest)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(AnonymousStandRequest $anonymousStandRequest)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, AnonymousStandRequest $anonymousStandRequest)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(AnonymousStandRequest $anonymousStandRequest)
+    {
+        //
+    }
+}
---
+++ app/Http/Controllers/InstitutionStandRequestController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\InstitutionStandRequest;
+use Illuminate\Http\Request;
+
+class InstitutionStandRequestController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(InstitutionStandRequest $institutionStandRequest)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(InstitutionStandRequest $institutionStandRequest)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, InstitutionStandRequest $institutionStandRequest)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(InstitutionStandRequest $institutionStandRequest)
+    {
+        //
+    }
+}
