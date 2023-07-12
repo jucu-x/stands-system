b966e4c172e8d5da6826f18e155bf237093f0e38
C-ArenA <carlostata.ca@gmail.com>
2023-07-11 21:38:42 -0400

    Base layout improved

--- resources/views/components/layouts/base.blade.php
+++ resources/views/components/layouts/base.blade.php
@@ -1,18 +1,22 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Stands</title>
     @vite('resources/scss/app.scss')
-    <script>
-
-    </script>
+    <script></script>
 </head>
-<body>
-    <x-dark-mode-toggle/>
-    {{$slot}}
+
+<body class="bg-white dark:bg-gray-900 dark:text-white">
+
+    <x-main-nav/>
+    <div class="pt-24">
+        {{ $slot }}
+    </div>
     @vite('resources/ts/app.ts')
 </body>
+
 </html>
