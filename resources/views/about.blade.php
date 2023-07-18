efe4fb3b1ae9e877af4da32604efe915f6fff634
c-arena <carlostata.ca@gmail.com>
2023-07-18 13:13:27 -0400

    About page created

---
+++ resources/views/about.blade.php
@@ -0,0 +1,7 @@
+<x-layouts.base>
+    <div class="grid place-items-center h-screen">
+        <p class="text-lg">
+            💚 Made by <a href="#" class="font-mono font-medium text-lime-300 underline">C-ArenA</a> from the Jucux team
+        </p>
+    </div>
+</x-layouts.base>
