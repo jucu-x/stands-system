e383d4f1598bdcafcffe54d4c03e1a9413e0d1dc
c-arena <carlostata.ca@gmail.com>
2023-07-18 13:15:53 -0400

    Improved counter component and fixed action-button component

--- resources/views/components/atomic/action-button.blade.php
+++ resources/views/components/atomic/action-button.blade.php
@@ -1,4 +1,4 @@
-@props(['href'=>route('stands.home'), ])
+@props(['href'=>'#', ])
 <div>
     <a href="{{ $href }}"
         class="mt-4 inline-flex items-center justify-center px-5 py-3 text-base font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900">
--- resources/views/components/form/counter.blade.php
+++ resources/views/components/form/counter.blade.php
@@ -1,4 +1,4 @@
-@props(['label' => 'Task', 'value'=>null, 'name' => 'title', 'placeholder'=>'Gobernar el mundo...'])
+@props(['label' => 'Task', 'value'=>null, 'name' => 'title', 'placeholder'=>'Gobernar el mundo...', 'min'=>null, 'max'=>null])
 <div>
     <label for="{{ $name }}"
         class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">{{ $label }}</label>
@@ -6,10 +6,14 @@
     @if ($value != null)
         <input value="{{ $value }}" name="{{ $name }}" type="number" id="{{ $name }}"
             class="block w-full p-4 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 sm:text-md focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            min="{{$min}}"
+            max="{{$max}}"
             placeholder="{{$placeholder}}" required>
     @else
         <input name="{{ $name }}" type="number" id="{{ $name }}"
             class="block w-full p-4 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 sm:text-md focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            min="{{$min}}"
+            max="{{$max}}"
             placeholder="{{$placeholder}}" required>
     @endif
 
