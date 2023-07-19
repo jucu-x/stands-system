ab0d096a30e46e47fbf618d78dacc3e2a1a8607f
c-arena <carlostata.ca@gmail.com>
2023-07-18 20:17:01 -0400

    Refactored stand controller

--- app/Http/Controllers/StandController.php
+++ app/Http/Controllers/StandController.php
@@ -8,35 +8,85 @@ use Illuminate\Http\Request;
 
 class StandController extends Controller
 {
-    public function standsHome()
-    {
-        $stands = Stand::all();
-        $expos = Expo::all();
-        return view('stands.stands-home', ['stands' => $stands, 'expos' => $expos]);
-    }
+
+    /**************************************************************
+     *                                                            *
+     *              RESOURCE CONTROLLER METHODS                   *
+     *                                                            *
+     **************************************************************/
+
     /**
-     * Display a listing of the resource.
+     * Display a listing of the stands.
      */
-    public function index()
+    public function index(Request $request)
     {
-
-        $expos = Expo::all();
-        if (request()->get('expo_id')) {
-            $selected_expo = Expo::find(request()->get('expo_id'));
-            if ($selected_expo==null) {
-                # code...
-                $stands = Stand::all();
-            } else {
-                $stands = Stand::where('expo_id', $selected_expo->id)->get();
-
-            }
+        $selected_expo = Expo::find($request->query('expo_id'));
+        if ($selected_expo) {
+            $stands = $selected_expo->stands;
         } else {
-            $selected_expo = null;
             $stands = Stand::all();
         }
-        return view('stands.index', ['stands' => $stands, 'expos' => $expos, 'selected_expo' => $selected_expo]);
+        return view('stands.index', [
+            'stands' => $stands,
+            'expos' => Expo::all(),
+            'selected_expo' => $selected_expo
+        ]);
     }
 
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Stand $stand)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Stand $stand)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Stand $stand)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Stand $stand)
+    {
+        //
+    }
+
+
+    /***********************************************
+     *                                             *
+     *              BULK OPERATIONS                *
+     *                                             *
+     ***********************************************/
 
     /**
      * Show the form for creating stands in bulk .
@@ -45,13 +95,6 @@ class StandController extends Controller
     {
         $expos = Expo::all();
         return view('stands.bulk-create', ['expos' => $expos, 'selected_expo' => $expo]);
-    }
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
     }
 
     /**
@@ -93,79 +136,9 @@ class StandController extends Controller
         }
         return redirect()->route('stands.index');
     }
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function initial_store(Request $request)
-    {
-        $normal_start = 1;
-        $normal_end = intval($request['normal-stands-count']);
-        $partial_start = $normal_end + 1;
-        $partial_end = $partial_start + $request['partial-stands-count'];
-        //dd($request, $normal_end, $partial_start,  $partial_end);
-        for ($i = $normal_start; $i <= $normal_end; $i++) {
-            # code...
-            Stand::create(
-                [
-                    "code" => $i,
-                    "partial_time" => false,
-                    "expo_id" => $request['expo_id']
-                ]
-            );
-        }
-        for ($i = $partial_start; $i <= $partial_end; $i++) {
-            # code...
-            Stand::create(
-                [
-                    "code" => $i,
-                    "partial_time" => true,
-                    "expo_id" => $request['expo_id']
-                ]
-            );
-        }
-        return redirect()->route('stands.home');
-    }
 
     /**
-     * Display the specified resource.
-     */
-    public function show(Stand $stand)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(Stand $stand)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, Stand $stand)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(Stand $stand)
-    {
-        //
-    }
-    /**
-     * Remove all
+     * Remove all STANDS in expo
      */
     public function destroyAllInExpo(Expo $expo)
     {
@@ -173,6 +146,15 @@ class StandController extends Controller
         return back();
     }
 
+    /*****************************************************
+     *                                                   *
+     * -   -   -   -   -   UTILITIES   -   -   -   -   - *
+     *                                                   *
+     *****************************************************/
+
+    /**
+     * Calcs codes to be assigned to the new stands created in bulk
+     */
     private function bulkCodesIntervals($normal_stands_amount, $partial_stands_amount, $last_code = 0): array
     {
         $normal_code_i = $last_code + 1;
