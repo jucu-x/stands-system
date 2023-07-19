98d5c6b2af4591404accd6e7f86faffc6177c36e
c-arena <carlostata.ca@gmail.com>
2023-07-18 23:53:29 -0400

    Created verbose fields functionality like in django

--- app/Models/Stand.php
+++ app/Models/Stand.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Stand extends Model
 {
@@ -21,6 +22,28 @@ class Stand extends Model
         'expo_id'
     ];
 
+    static public $verbose_fields = [
+        'code' => 'Código',
+        'partial_time' => 'De tiempo parcial',
+        'expected_cost' => 'Costo sugerido',
+        'building' => 'Edificio o sección',
+        'x' => 'x',
+        'y' => 'y',
+        'width' => 'Ancho del stand',
+        'length' => 'Largo del stand',
+        'expo_id' => 'Id del evento',
+        'created_by' => 'Creado por',
+        'updated_by' => 'Última vez modificado por',
+        'created_at' => 'Fecha de creación',
+        'updated_at' => 'Fecha de última modificación',
+        // Custom fields
+        'expo' => 'Evento',
+    ];
+    public function expo(): BelongsTo
+    {
+        return $this->belongsTo(Expo::class);
+    }
+
     /**
      * Finds the last created stand's code for any specific expo
      * @param int $expo_id expo->id
@@ -29,5 +52,13 @@ class Stand extends Model
     static public function lastCodeInExpo($expo_id): int
     {
         return intval(self::where('expo_id', $expo_id)->orderBy('code', 'desc')->first()?->code);
+    }
+
+    static public function fieldsToVerbose(array $fields_array): array{
+        $verbose_fields_array = [];
+        foreach ($fields_array as $field) {
+            array_push($verbose_fields_array, self::$verbose_fields[$field]);
+        }
+        return $verbose_fields_array;
     }
 }
