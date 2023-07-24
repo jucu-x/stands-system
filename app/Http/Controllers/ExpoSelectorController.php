9d3611ddd43e0a71d14bfa083ff59139a7d95a24
c-arena <carlostata.ca@gmail.com>
2023-07-24 01:52:24 -0400

    Made methods to interact with the expo selector and fixed contoller

--- app/Http/Controllers/ExpoSelectorController.php
+++ app/Http/Controllers/ExpoSelectorController.php
@@ -2,9 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Enums\ExpoSelectorCriteria;
 use App\Http\Requests\StoreExpoSelectorRequest;
 use App\Http\Requests\UpdateExpoSelectorRequest;
 use App\Models\ExpoSelector;
+use Illuminate\Http\Request;
 
 class ExpoSelectorController extends Controller
 {
@@ -27,7 +29,7 @@ class ExpoSelectorController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(StoreExpoSelectorRequest $request)
+    public function store(Request $request)
     {
         //
     }
@@ -51,7 +53,7 @@ class ExpoSelectorController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(UpdateExpoSelectorRequest $request, ExpoSelector $expoSelector)
+    public function update(Request $request, ExpoSelector $expoSelector)
     {
         //
     }
--- app/Models/ExpoSelector.php
+++ app/Models/ExpoSelector.php
@@ -2,10 +2,25 @@
 
 namespace App\Models;
 
+use App\Enums\ExpoSelectorCriteria;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class ExpoSelector extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'criterion',
+        'expo_id'
+    ];
+    public function expo(): BelongsTo
+    {
+        return $this->belongsTo(Expo::class);
+    }
+
+    static public function current() : ExpoSelector|null {
+        return self::where('criterion', ExpoSelectorCriteria::Current->value)->first();
+    }
 }
