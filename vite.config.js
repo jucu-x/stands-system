0f9568eecf3ba339c28824d41232e897d898fabf
c-arena <carlostata.ca@gmail.com>
2023-07-26 12:29:11 -0400

    breeze scafolding revision: config files

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,12 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/scss/app.scss', 'resources/ts/app.ts'],
+            input: [
+                'resources/scss/app.scss',
+                'resources/ts/app.ts',
+                'resources/css/app.css',
+                'resources/js/app.js',
+            ],
             refresh: true,
         }),
     ],
