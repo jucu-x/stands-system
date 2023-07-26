5ef3a71fb479f8e1da1c91e708797098bb0679d5
c-arena <carlostata.ca@gmail.com>
2023-07-26 12:36:17 -0400

    breeze scafoling revision: css and js

--- resources/ts/app.ts
+++ resources/ts/app.ts
@@ -1,3 +1,8 @@
 import 'flowbite';
 import './components';
-import 'remixicon/fonts/remixicon.css'
+import 'remixicon/fonts/remixicon.css';
+import Alpine from 'alpinejs';
+
+window.Alpine = Alpine;
+
+Alpine.start();
